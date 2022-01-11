f5098711964d8879a519d38689e1bcbef1e0843b
Jeff <jeffrey.estella@gmail.com>
2022-01-10 19:13:32 -0800

    PhotoBox: Build and style. App: Adjust alignment. CTABtn: tweak shadow

--- src/components/PhotoBox.js
+++ src/components/PhotoBox.js
@@ -1,20 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import photoBoxStyles from './PhotoBox.module.css';
+import productPhoto1 from '../assets/image-product-1.jpg';
+import productPhoto2 from '../assets/image-product-2.jpg';
+import productPhoto3 from '../assets/image-product-3.jpg';
+import productPhoto4 from '../assets/image-product-4.jpg';
+import productThumb1 from '../assets/image-product-1-thumbnail.jpg';
+import productThumb2 from '../assets/image-product-2-thumbnail.jpg';
+import productThumb3 from '../assets/image-product-3-thumbnail.jpg';
+import productThumb4 from '../assets/image-product-4-thumbnail.jpg';
 
 const PhotoBox = () => {
+    const photos = [
+        {
+            photo: productPhoto1,
+            thumb: productThumb1,
+            alt: "white and brown sneakers"
+        },
+        {
+            photo: productPhoto2,
+            thumb: productThumb2,
+            alt: "white and brown sneakers"
+        },
+        {
+            photo: productPhoto3,
+            thumb: productThumb3 ,
+            alt: "white and brown sneakers"
+        },
+        {
+            photo: productPhoto4,
+            thumb: productThumb4,
+            alt: "white and brown sneakers"
+        },
+    ]
+
+    const [currentPhoto, setCurrentPhoto] = useState(productPhoto1);
+
+    const thumbs = photos.map((photo) => {
+        return (
+            <img 
+                className={`
+                    ${currentPhoto===photo.photo?
+                        photoBoxStyles.selected :
+                        ''
+                    } ${photoBoxStyles.thumbnail}`}
+                src={photo.thumb} 
+                alt={photo.alt} 
+                onClick={() => {setCurrentPhoto(photo.photo)}}
+            />
+        )
+    })
+
     return(
         <div className={photoBoxStyles.container}>
             <div>
-                Main Photo
+                <img 
+                    className={photoBoxStyles.current}
+                    src={currentPhoto} 
+                    alt="pair of white sneakers and brown sneakers with orange accents"
+                />
             </div>
-            <div>
-                <span>Photo1</span>
-                <span>Photo2</span>
-                <span>Photo3</span>
-                <span>Photo4</span>
+            <div className={photoBoxStyles.thumbnails}>
+                {thumbs}
             </div>
         </div>
     );
 }
 
 export default PhotoBox;
+
